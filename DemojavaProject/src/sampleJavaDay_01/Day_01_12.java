acedf95868c17c3995254e39dbb5198612c52f24
Dinesh <dkg44842gmail.com>
2023-03-12 13:10:13 +0530

    Third Commit

--- DemojavaProject/src/sampleJavaDay_01/Day_01_12.java
+++ DemojavaProject/src/sampleJavaDay_01/Day_01_12.java
@@ -1,5 +1,12 @@
 package sampleJavaDay_01;
 
 public class Day_01_12 {
+	
+	public static void main(String[] args)
+	{
+		int a = 7;
+		int b = 6;
+		System.out.println("Addition of two numbers: " + (a+b));
+	}
 
 }
--- DemojavaProject/src/sampleJavaDay_01/DemoClass.java
+++ DemojavaProject/src/sampleJavaDay_01/DemoClass.java
@@ -1,5 +1,12 @@
 package sampleJavaDay_01;
 
 public class DemoClass {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("High");
+		System.out.println("Higher");
+		System.out.println("Imtermediate");System.out.println("College");
+	}
 
 }
