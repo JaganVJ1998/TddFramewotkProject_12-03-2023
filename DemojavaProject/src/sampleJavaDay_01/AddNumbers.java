6e4d018bc27cd28c69ca945ae2f18051b2de9d1f
Dinesh <dkg44842gmail.com>
2023-03-12 12:58:22 +0530

    Second commit

---
+++ DemojavaProject/src/sampleJavaDay_01/AddNumbers.java
@@ -0,0 +1,13 @@
+package sampleJavaDay_01;
+
+public class AddNumbers {
+	
+	public static void main(String [] args)
+	{
+		
+		int a = 7;
+		int b = 6;
+		System.out.println("Addition of two numbers: " + (a+b));
+	}
+
+}
---
+++ DemojavaProject/src/sampleJavaDay_01/Stringdemo.java
@@ -0,0 +1,18 @@
+package sampleJavaDay_01;
+
+public class Stringdemo {
+	
+	public static void main(String [] args)
+	{
+		
+		int a = 7;
+		int b = 6;
+		System.out.println("Addition of two numbers: " + (a+b));
+		
+		String s = "Welcom";
+		s.concat(" All");
+		System.out.println(s);
+	}
+
+
+}
